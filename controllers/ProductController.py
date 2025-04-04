2dbe6c17c7796b2cdd9261e9cf3c49bbb4e42544
milankyada1997 <milankyada006@gmail.com>
2025-04-04 12:40:53 +0530

    .

--- controllers/ProductController.py
+++ controllers/ProductController.py
@@ -1,18 +1,3 @@
-# from models.ProductModel import Product
-# from config.database import product_collection
-# from fastapi import APIRouter, HTTPException
-# from fastapi.responses import JSONResponse  
-# from bson import ObjectId
-
-# async def create_product(product: Product):
-#     print(product.dict())
-#     product.category_id = ObjectId(product.category_id)
-#     product.sub_category_id = ObjectId(product.sub_category_id)
-#     product.seller_id = ObjectId(product.seller_id)
-
-#     savedProduct = await product_collection.insert_one(product.dict())
-#     return JSONResponse(content={"message": "Product created successfully"}, status_code=201)
-
 from models.ProductModel import Product,ProductOut
 from config.database import product_collection,category_collection,user_collection,sub_category_collection
 from fastapi import APIRouter, HTTPException, UploadFile, File,Form
